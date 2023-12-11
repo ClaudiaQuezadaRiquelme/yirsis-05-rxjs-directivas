11559e80ceeb4bbad5fdaa413d54ad607c5580b9
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-11 20:48:28 -0300

    subscribe deprecado (=< angular 12)

--- src/app/reactive/reactive-container/reactive-container.component.ts
+++ src/app/reactive/reactive-container/reactive-container.component.ts
@@ -8,19 +8,35 @@ import { Observable } from 'rxjs';
 })
 export class ReactiveContainerComponent implements OnInit {
   num: number = 0;
+  error: string = '';
+  complete: string = '';
 
   constructor() {
     const miObservable = new Observable<number>((observer)=> {
       let numObservable = 0;
       setInterval(()=> {
         numObservable++;
-        observer.next(numObservable);   
-      }, 1500);
+        observer.next(numObservable);
+        observer.complete();
+        if (numObservable === 3) {
+          observer.error('Número erróneo.');
+        }
+      }, 2000);
     });
 
-    miObservable.subscribe((result)=> {
-      this.num = result;
-    });
+    miObservable.subscribe(
+      (result)=> {
+        this.num = result;
+      },
+      (error) => {
+        console.log('error:', error);
+        
+        this.error = error;
+      },
+      () => { // complete
+        this.complete = `Completado con num: ${this.num}`;
+      }
+    );
   }
 
   ngOnInit(): void {
