01409209791b31d34b03f4d41fb260792dbdc8b5
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-11 14:57:57 -0300

    reactive-container component

---
+++ src/app/reactive/reactive-container/reactive-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reactive-container',
+  templateUrl: './reactive-container.component.html',
+  styleUrls: ['./reactive-container.component.css']
+})
+export class ReactiveContainerComponent {
+
+}
