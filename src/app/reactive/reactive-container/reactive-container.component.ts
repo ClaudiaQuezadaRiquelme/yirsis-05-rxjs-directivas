3357d2e8dab1c0421a7a6b22ac2b034cae7b7fde
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-11 21:32:05 -0300

    show subscribe deprecado

--- src/app/reactive/reactive-container/reactive-container.component.ts
+++ src/app/reactive/reactive-container/reactive-container.component.ts
@@ -7,11 +7,23 @@ import { Observable } from 'rxjs';
   styleUrls: ['./reactive-container.component.css']
 })
 export class ReactiveContainerComponent implements OnInit {
+
+  numSubscribeDeprecado: number = 0;
+  errorSubscribeDeprecado: string = '';
+  completeSubscribeDeprecado: string = '';
+  completarSD: boolean = false;
+  errorSD: boolean = false;
+  reiniciarSD: boolean = false;
+
   num: number = 0;
   error: string = '';
   complete: string = '';
 
   constructor() {
+    this.executeMiObservableDeprecado();
+    
+
+
     const miObservable = new Observable<number>((observer)=> {
       let numObservable = 0;
       setInterval(()=> {
@@ -41,5 +53,66 @@ export class ReactiveContainerComponent implements OnInit {
 
   ngOnInit(): void {
       
+  }
+
+  executeMiObservableDeprecado() {
+    const miObservableDeprecado = new Observable<number>((observer)=> {
+      let numObservableDeprecado = 0;
+      setInterval(()=> {
+        if (this.reiniciarSD) {
+          numObservableDeprecado = 0;
+          this.reiniciarSD = false;
+        }
+        numObservableDeprecado++;
+        observer.next(numObservableDeprecado);
+        
+        if (this.completarSD) {
+          observer.complete();
+        }
+        if (this.errorSD) {
+          observer.error('Número erróneo.');
+        }
+      }, 1500);
+    });
+
+    miObservableDeprecado.subscribe(
+      (result)=> {
+        this.numSubscribeDeprecado = result;
+      },
+      (error) => {
+        console.log('error:', error);
+        
+        this.errorSubscribeDeprecado = error;
+      },
+      () => { // complete
+        this.completeSubscribeDeprecado = `Completado con num: ${this.numSubscribeDeprecado}`;
+      }
+    );
+
+  }
+
+  variablesSubscribeDeprecadoReiniciar() {
+    this.completarSD = false;
+    this.errorSD = false;
+    this.errorSubscribeDeprecado = '';
+    this.completeSubscribeDeprecado = '';
+  }
+
+  reiniciarSubscribeDeprecado() {
+    this.reiniciarSD = true;
+    if (this.errorSD || this.completarSD) { // executeAgainSD
+      this.numSubscribeDeprecado = 0;
+      this.reiniciarSD = false;
+      this.variablesSubscribeDeprecadoReiniciar();
+      this.executeMiObservableDeprecado();
+    }
+  }
+
+  toCompleteSubscribeDeprecado() {
+    this.completarSD = true;
+  }
+
+  toErrorSubscribeDeprecado() {
+    this.errorSD = true;
   }
 }
