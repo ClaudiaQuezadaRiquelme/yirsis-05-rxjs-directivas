db797f4f676ecb369ac6fc76984d116ffe830cb7
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-14 10:23:04 -0300

    theme directive test

--- src/app/directives/theme.directive.spec.ts
+++ src/app/directives/theme.directive.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ThemeDirective } from './theme.directive';
 import { AppComponent } from '../app.component';
 import { ReactiveModule } from '../reactive/reactive.module';
+import { By } from '@angular/platform-browser';
 
 describe('ThemeDirective', () => {
   let component: AppComponent;
@@ -24,5 +25,11 @@ describe('ThemeDirective', () => {
 
   it('should create an instance', () => {
     expect(component).toBeTruthy();
+  });
+  it('Debe crear un DIV con el Background #333 y el color "snow"', () => {
+    const elem: HTMLDivElement = fixture.debugElement.query(By.directive(ThemeDirective)).nativeElement;
+    expect(elem).toBeTruthy();
+    expect(elem.style.backgroundColor).toEqual('rgb(51, 51, 51)'); // #333
+    expect(elem.style.color).toEqual('snow'); // #333
   });
 });
