16e2f23e3190013596485f31023965c2b23e6aec
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-14 16:35:13 -0300

    area mouse directive test

--- src/app/reactive/directives/area-mouse.directive.spec.ts
+++ src/app/reactive/directives/area-mouse.directive.spec.ts
@@ -1,8 +1,32 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AreaMouseDirective } from './area-mouse.directive';
+import { ReactiveContainerComponent } from '../reactive-container/reactive-container.component';
+import { ReactiveModule } from '../reactive.module';
+import { By } from '@angular/platform-browser';
 
 describe('AreaMouseDirective', () => {
+  let component: ReactiveContainerComponent;
+  let fixture: ComponentFixture<ReactiveContainerComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [
+        ReactiveContainerComponent,
+        AreaMouseDirective
+      ]
+    });
+    fixture = TestBed.createComponent(ReactiveContainerComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
   it('should create an instance', () => {
-    // const directive = new AreaMouseDirective();
-    // expect(directive).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+  it('Debe crear un DIV con el Background "blue" y tamaño "400px"', () => {
+    const elem: HTMLButtonElement = fixture.debugElement.query(By.directive(AreaMouseDirective)).nativeElement;
+    expect(elem).toBeTruthy();
+    expect(elem.style.backgroundColor).toEqual('blue');
+    expect(elem.style.height).toEqual('400px');
   });
 });
